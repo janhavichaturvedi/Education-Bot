b5939134ad3993a0fb096b654b68620dc03f9bd5
Gayathri Sathish <43489150+gayathri1902@users.noreply.github.com>
2022-09-27 23:08:10 +0530

    Add files via upload

---
+++ Chatbot page/constants.js
@@ -0,0 +1,17 @@
+
+const prompts = [
+    ["hi", "hey", "hello", "good morning", "good afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"]
+  ]
+  
+  const replies = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!"],
+    ["Fine... how are you?","Pretty well, how are you?","Fantastic, how are you?"],
+    ["Nothing much","About to go to sleep","Can you guess?","I don't know actually"]
+  ]
+
+  const alternative = ["Try again","I'm listening...","I don't understand :/"]
+  
+
+  
